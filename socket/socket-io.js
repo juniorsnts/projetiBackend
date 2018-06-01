a6a0fc4555f6829595a25cd17e3722ba5336f233
juniorsnts123@gmail.com <juniorsnts123@gmail.com>
2018-05-31 23:05:01 -0300

    pacote responsavel pela logica do socket.io

---
+++ socket/socket-io.js
@@ -0,0 +1,23 @@
+const formData = require('../formData');
+let io = null;
+var obj = {
+    connect: function(http){
+        io = require('socket.io')(http);
+        io.on('connection', (socket) => {
+            socket.on('disconnect', function(){
+                var formhora =  formData("hora");
+                console.log('[' + formhora + '] disconectou do socket: ' + socket.id);
+            });
+            var formhora =  formData("hora");
+            console.log('[' + formhora + '] conectado ao socket: ' +  socket.id);
+        });
+    },
+
+    emit: function(valores){
+        if(io != null){
+            io.emit('valores',valores);
+        }
+    }
+}
+
+module.exports = obj;
